b0b1c38f9f0fed2e5cfcf545d87efdc5a846b9ba
Aluno <Aluno@SNCTM202LABF04>
2023-08-10 22:21:10 -0300

    Adicionar arquivos de projeto.

---
+++ SaveAddress/SaveAddress/Pages/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SaveAddress
+{
+    public partial class MainPage : FlyoutPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        void Navegar(ContentPage page)
+        {
+            Detail = new NavigationPage(page);
+            IsPresented = false;
+        }
+    }
+}
